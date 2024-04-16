93a172c48756e8e629d4743e416f2a27f373e756
JasonWolterDeveloper <143658631+JasonWolterDeveloper@users.noreply.github.com>
2024-04-16 15:49:38 -0500

    Migrate Button to Tailwin

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,28 +1,13 @@
 import { useState } from 'react';
 import { styled } from 'styled-components'; 
 import CustomInput from './CustomInput';
+import Button from './Button';
 
 const ControlDiv = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `
 
 export default function AuthInputs() {
